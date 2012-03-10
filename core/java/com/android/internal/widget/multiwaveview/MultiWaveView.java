d4bf9e61e7eefa8df503193a2115270d6d22329a
shane6374 <shane.ashby63@gmail.com>
2012-03-10 16:06:48 -0600

    Update core/java/com/android/internal/widget/multiwaveview/MultiWaveView.java

--- core/java/com/android/internal/widget/multiwaveview/MultiWaveView.java
+++ core/java/com/android/internal/widget/multiwaveview/MultiWaveView.java
@@ -70,8 +70,8 @@ public class MultiWaveView extends View {
     }
 
     // Tune-able parameters
-    private static final int CHEVRON_INCREMENTAL_DELAY = 160;
-    private static final int CHEVRON_ANIMATION_DURATION = 850;
+    private static final int CHEVRON_INCREMENTAL_DELAY = 6400;
+    private static final int CHEVRON_ANIMATION_DURATION = 4000;
     private static final int RETURN_TO_HOME_DELAY = 1200;
     private static final int RETURN_TO_HOME_DURATION = 300;
     private static final int HIDE_ANIMATION_DELAY = 200;
@@ -307,12 +307,12 @@ public class MultiWaveView extends View {
         final float chevronAnimationDistance = mOuterRadius * 0.9f;
         final float from[][] = {
                 {mWaveCenterX - r, mWaveCenterY},  // left
-                {mWaveCenterX + r, mWaveCenterY},  // right
+                {mWaveCenterX, mWaveCenterY},  // right
                 {mWaveCenterX, mWaveCenterY - r},  // top
                 {mWaveCenterX, mWaveCenterY + r} }; // bottom
         final float to[][] = {
                 {mWaveCenterX - chevronAnimationDistance, mWaveCenterY},  // left
-                {mWaveCenterX + chevronAnimationDistance, mWaveCenterY},  // right
+                {mWaveCenterX, mWaveCenterY},  // right
                 {mWaveCenterX, mWaveCenterY - chevronAnimationDistance},  // top
                 {mWaveCenterX, mWaveCenterY + chevronAnimationDistance} }; // bottom
 
